b3337d303075cf83b85eda4eb50e7be892744179
MashR3 <michaelash17@googlemail.com>
2017-10-04 17:14:43 +0100

    Successful Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,10 @@
 Rails.application.routes.draw do
-  get 'comments/index'
+ 
+ resources :posts do 
+  resources :comments
+  
+  end
 
-  get 'comments/show'
+    root "posts#index"
 
-  get 'comments/new'
-
-  get 'comments/create'
-
-  get 'comments/edit'
-
-  get 'comments/update'
-
-  get 'comments/destroy'
-
-  get 'posts/index'
-
-  get 'posts/show'
-
-  get 'posts/new'
-
-  get 'posts/create'
-
-  get 'posts/edit'
-
-  get 'posts/update'
-
-  get 'posts/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
