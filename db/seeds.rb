88efe3b8909dce73eb0b24309610ee11d85bce93
MashR3 <michaelash17@googlemail.com>
2017-10-04 17:04:04 +0100

    Model Test working with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p1 = Post.create!(name: "I'm a winner!", author: "Julia Roberts", theme: "Actor Life", body: "Today I went to the Oscars. I won an award for Pretty Woman. I deserved it. I played a hooker. And my hair is as bright as a thousand flames.", picture_url: "https://a.dilcdn.com/bl/wp-content/uploads/sites/25/2015/01/oscars.jpg")
+
+c1 = Comment.create!(name: "Michael Ash", body: "I love you! Congratulations!", location: "Croydon", email_address: "Mash@hotmail.com", age: 24, post_id:p1.id)
+c2 = Comment.create!(name: "Jack Scott", body: "Congratulations! I love you more than Mash!", location: "Vauxhall", email_address: "Jack@hotmail.com", age: 24, post_id:p1.id)
